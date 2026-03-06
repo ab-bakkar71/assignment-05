a737b5b915d7fb5ee6b0d60f11fc7c198195fc3b
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-03-07 00:57:43 +0600

    login page and nav bar done

---
+++ js/login.js
@@ -0,0 +1,19 @@
+// get The element
+document.getElementById('input-btn').addEventListener('click', function(){
+    // get the input value
+    const inputUserName = document.getElementById('input-username');
+    const userName = inputUserName.value;
+    const inputPassword = document.getElementById('input-password');
+    const password = inputPassword.value;
+    
+
+    // Login conation
+    if(userName == "admin" && password == "admin123"){
+        alert('SingIn Success');
+        window.location.assign('./main.html')
+    }
+    else{
+        alert('SingIn Failed')
+        return;
+    }
+});
