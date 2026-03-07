e38f8f5d310ed0cdd887e7cbc756e697b4c805a2
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-03-07 21:34:07 +0600

    modal funcation added

--- js/script.js
+++ js/script.js
@@ -1,19 +1,67 @@
-// function for labels
-
+let issues = [];
+let currentTab = 'all';
+const tabActive = ["bg-[#4A00FF]", "border-none","text-white" ];
+const tabInActive = ["bg-transparent", "border-[#E4E4E7]", "text-[#64748B" ]
 
 // All card Api
 const AllIssues = () =>{
  fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
  .then(res=> res.json())
- .then(data => issuesDisplay(data.data))
+ .then(data => {
+   issues = data.data;
+   displayIssues();
+ })
+}
+
+// issues details modal
+const issuesDetail = async(id) => {
+  const detailUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res =await fetch(detailUrl);
+  const details = await res.json();
+  displayIssuesDetails(details.data)
+  
+}
+
+// display issues details
+
+const displayIssuesDetails = (issue) => {
+  console.log(issue)
+  const detailsModal = document.getElementById('details-container')
+  detailsModal.innerHTML="This is modal"
+
+  document.getElementById('modal_details').showModal();
+}
+
+const displayIssues = () => {
+  let filterIssues = issues;
+  
+  if(currentTab === 'open') {
+    filterIssues = issues.filter(issue => issue.status === 'open');
+  } else if(currentTab === 'closed') {
+    filterIssues = issues.filter(issue => issue.status === 'closed');
+  }
+  
+  issuesDisplay(filterIssues);
 }
 
 // all card display
 const issuesDisplay = (issues) => {
-    const issuesContainer =document.getElementById('issues-container');
+    const issuesContainer = document.getElementById('issues-container');
     const allCount = document.getElementById('all-count');
-    allCount.innerText=issues.length;
-    issuesContainer.innerHTML= "";
+    
+    // issue count
+    if(currentTab === 'all') {
+      allCount.innerText = issues.length + ' Issues';
+    } else if(currentTab === 'open') {
+      const openCount = issues.filter(i => i.status === 'open').length;
+      allCount.innerText = openCount + '  Issues';
+    } else if(currentTab === 'closed') {
+      const closedCount = issues.filter(i => i.status === 'closed').length;
+      allCount.innerText = closedCount + '  Issues';
+    }
+
+    issuesContainer.innerHTML = "";
+    
     for(let issue of issues){
         const issueCard = document.createElement('div');
         // border change with status
@@ -21,12 +69,11 @@ const issuesDisplay = (issues) => {
             : "border-t-4 border-[#A855F7]";
 
         // Icon change with status
-
         const changeIcon = issue.status == "open" ? `<img src="./assets/Open-Status.png" alt=""></img>` : `<img src="./assets/Closed- Status .png" alt=""></img>`
 
         // all card Show
         issueCard.innerHTML =`
-        <div class="card bg-base-100 w-full shadow-sm ${borderClass} ">
+        <div class="card bg-base-100 w-full shadow-sm ${borderClass}">
         <div class="card-body">
           <div class="flex justify-between items-center">
             ${changeIcon}
@@ -49,24 +96,19 @@ const issuesDisplay = (issues) => {
       </div>
         `
 
-       
-
+        issueCard.onclick = () =>displayIssuesDetails();
         issuesContainer.append(issueCard);
     }
 }
 
 // toggle btn function
-let  currentTab = 'all';
-const tabActive = ["bg-[#4A00FF]", "border-none","text-white" ];
-const tabInActive = ["bg-transparent", "border-[#E4E4E7]", "text-[#64748B" ]
-
 function switchTab(tab){
+  currentTab = tab;
   const tabs = ["all", "open", "closed"];
 
   for(const t of tabs){
     const tabName = document.getElementById("tab-"+ t);
     if(t === tab){
-     
       tabName.classList.remove(...tabInActive);
       tabName.classList.add(...tabActive);
     }
@@ -75,7 +117,10 @@ function switchTab(tab){
       tabName.classList.add(...tabInActive)
     }
   }
-
+  
+  // Display filtered issues
+  displayIssues();
 }
+
 switchTab(currentTab);
-AllIssues()
+AllIssues();
