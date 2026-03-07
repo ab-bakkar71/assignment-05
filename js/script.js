218e7bfc90293a817efe58106298653ef35ff203
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-03-07 23:26:56 +0600

    modal funcation and style done

--- js/script.js
+++ js/script.js
@@ -1,79 +1,139 @@
 let issues = [];
-let currentTab = 'all';
-const tabActive = ["bg-[#4A00FF]", "border-none","text-white" ];
-const tabInActive = ["bg-transparent", "border-[#E4E4E7]", "text-[#64748B" ]
+let currentTab = "all";
+const tabActive = ["bg-[#4A00FF]", "border-none", "text-white"];
+const tabInActive = ["bg-transparent", "border-[#E4E4E7]", "text-[#64748B"];
 
 // All card Api
-const AllIssues = () =>{
- fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
- .then(res=> res.json())
- .then(data => {
-   issues = data.data;
-   displayIssues();
- })
-}
+const AllIssues = () => {
+  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res) => res.json())
+    .then((data) => {
+      issues = data.data;
+      displayIssues();
+    });
+};
 
 // issues details modal
-const issuesDetail = async(id) => {
+const issuesDetail = async (id) => {
   const detailUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
-  const res =await fetch(detailUrl);
+  const res = await fetch(detailUrl);
   const details = await res.json();
-  displayIssuesDetails(details.data)
-  
-}
+  displayIssuesDetails(details.data);
+};
 
 // display issues details
 
 const displayIssuesDetails = (issue) => {
-  console.log(issue)
-  const detailsModal = document.getElementById('details-container')
-  detailsModal.innerHTML="This is modal"
+  // status change with color
+const changeBg =
+      issue.status == "open"
+        ? "text-white bg-emerald-500"
+        : "text-white bg-purple-500"
 
-  document.getElementById('modal_details').showModal();
-}
+  const detailsModal = document.getElementById("details-container");
+  detailsModal.innerHTML = `
+          <div>
+            <h1 class="font-bold text-2xl text-[#1F2937]">
+              ${issue.title}
+            </h1>
+            <div class="flex items-center gap-3 text-sm text-slate-500 my-3">
+              <span
+                class="px-3 py-1 ${changeBg} rounded-full text-xs font-medium"
+                >${issue.status}</span
+              >
+              <span>•</span>
+              <span>
+                Opened by
+                <span class="text-slate-700 font-medium">${issue.author}</span>
+              </span>
+              <span>•</span>
+              <span>${issue.createdAt.split("T")[0]}</span>
+            </div>
+            <div>
+              <p
+                class="badge badge-outline badge-error font-medium text-[#EF4444] bg-[#FEECEC]"
+              >
+                ${issue.labels[0]}
+              </p>
+              <p
+                class="badge badge-outline badge-warning font-medium text-[#D97706] bg-[#FDE68A]"
+              >
+               ${issue.labels[1]}
+              </p>
+            </div>
+            <p class="text-[#64748B] my-6">
+             ${issue.description}
+            </p>
+
+            <div
+              class="bg-gray-100 rounded-lg p-4 flex justify-between items-center"
+            >
+              <div>
+                <p class="text-gray-500 text-sm">Assignee:</p>
+                <p class="font-semibold text-gray-800">${issue.assignee}</p>
+              </div>
+              <div class="text-right">
+                <p class="text-gray-500 text-sm">Priority:</p>
+                <span
+                  class="px-3 py-1 text-xs font-semibold text-white bg-red-500 rounded-full"
+                >
+                  ${issue.priority}
+                </span>
+              </div>
+            </div>
+          </div>
+  `;
+
+  document.getElementById("modal_details").showModal();
+};
 
 const displayIssues = () => {
   let filterIssues = issues;
-  
-  if(currentTab === 'open') {
-    filterIssues = issues.filter(issue => issue.status === 'open');
-  } else if(currentTab === 'closed') {
-    filterIssues = issues.filter(issue => issue.status === 'closed');
+
+  if (currentTab === "open") {
+    filterIssues = issues.filter((issue) => issue.status === "open");
+  } else if (currentTab === "closed") {
+    filterIssues = issues.filter((issue) => issue.status === "closed");
   }
-  
+
   issuesDisplay(filterIssues);
-}
+};
 
 // all card display
 const issuesDisplay = (issues) => {
-    const issuesContainer = document.getElementById('issues-container');
-    const allCount = document.getElementById('all-count');
-    
-    // issue count
-    if(currentTab === 'all') {
-      allCount.innerText = issues.length + ' Issues';
-    } else if(currentTab === 'open') {
-      const openCount = issues.filter(i => i.status === 'open').length;
-      allCount.innerText = openCount + '  Issues';
-    } else if(currentTab === 'closed') {
-      const closedCount = issues.filter(i => i.status === 'closed').length;
-      allCount.innerText = closedCount + '  Issues';
-    }
+  const issuesContainer = document.getElementById("issues-container");
+  const allCount = document.getElementById("all-count");
 
-    issuesContainer.innerHTML = "";
-    
-    for(let issue of issues){
-        const issueCard = document.createElement('div');
-        // border change with status
-        const borderClass = issue.status == "open" ? "border-t-4 border-[#00A96E]"
-            : "border-t-4 border-[#A855F7]";
+  // issue count
+  if (currentTab === "all") {
+    allCount.innerText = issues.length + " Issues";
+  } else if (currentTab === "open") {
+    const openCount = issues.filter((i) => i.status === "open").length;
+    allCount.innerText = openCount + "  Issues";
+  } else if (currentTab === "closed") {
+    const closedCount = issues.filter((i) => i.status === "closed").length;
+    allCount.innerText = closedCount + "  Issues";
+  }
 
-        // Icon change with status
-        const changeIcon = issue.status == "open" ? `<img src="./assets/Open-Status.png" alt=""></img>` : `<img src="./assets/Closed- Status .png" alt=""></img>`
+  issuesContainer.innerHTML = "";
 
-        // all card Show
-        issueCard.innerHTML =`
-        <div class="card bg-base-100 w-full shadow-sm ${borderClass}">
+  for (let issue of issues) {
+    const issueCard = document.createElement("div");
+    // border change with status
+    const borderClass =
+      issue.status == "open"
+        ? "border-t-4 border-[#00A96E]"
+        : "border-t-4 border-[#A855F7]";
+
+    // Icon change with status
+    const changeIcon =
+      issue.status == "open"
+        ? `<img src="./assets/Open-Status.png" alt=""></img>`
+        : `<img src="./assets/Closed- Status .png" alt=""></img>`;
+
+    // all card Show
+    issueCard.innerHTML = `
+        <div class="card bg-base-100 w-full shadow-sm cursor-pointer ${borderClass}">
         <div class="card-body">
           <div class="flex justify-between items-center">
             ${changeIcon}
@@ -90,34 +150,33 @@ const issuesDisplay = (issues) => {
           <hr class="w-full block border-[#E4E4E7] my-4">
           <div class="space-y-2.5 text-[#64748B]">
             <p># ${issue.id} ${issue.author}</p>
-            <p>${issue.createdAt}</p>
+            <p>${issue.createdAt.split("T")[0]}</p>
           </div>
         </div>
       </div>
-        `
+        `;
 
-        issueCard.onclick = () =>displayIssuesDetails();
-        issuesContainer.append(issueCard);
-    }
-}
+    issueCard.onclick = () => issuesDetail(issue.id);
+    issuesContainer.append(issueCard);
+  }
+};
 
 // toggle btn function
-function switchTab(tab){
+function switchTab(tab) {
   currentTab = tab;
   const tabs = ["all", "open", "closed"];
 
-  for(const t of tabs){
-    const tabName = document.getElementById("tab-"+ t);
-    if(t === tab){
+  for (const t of tabs) {
+    const tabName = document.getElementById("tab-" + t);
+    if (t === tab) {
       tabName.classList.remove(...tabInActive);
       tabName.classList.add(...tabActive);
-    }
-    else{
+    } else {
       tabName.classList.remove(...tabActive);
-      tabName.classList.add(...tabInActive)
+      tabName.classList.add(...tabInActive);
     }
   }
-  
+
   // Display filtered issues
   displayIssues();
 }
